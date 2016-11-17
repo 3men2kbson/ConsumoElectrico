c2d2300949c049ac031a2ec6b088fee8757402da
3men2kbson <lazarogs@gmail.com>
2016-11-16 22:43:08 -0400

    Agregado el Modelo del Precio de la Energía

---
+++ ConsumoElectrico/Models/PrecioEnergia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace ConsumoElectrico.Models
+{
+    public class PrecioEnergia
+    {
+        [DisplayName("Id")]
+        public int PEId { get; set;  }
+        [DisplayName("Descripción")]
+        public string PEDesc { get; set; }
+        [DisplayName("Desde")]
+        public int PEDesde { get; set; }
+        [DisplayName("Hasta")]
+        public int PEhasta { get; set; }
+        [DisplayName("Sin Subsidio")]
+        public decimal PESinSubs { get; set; }
+        [DisplayName("Con Subsidio")]
+        public decimal PEConsubs { get; set; }
+        [DisplayName("Activo Desde")]
+        public DateTime FechaActivo { get; set; }
+        [DisplayName("Activo Hasta")]
+        public DateTime FechaLimite { get; set; }
+    }
+}
