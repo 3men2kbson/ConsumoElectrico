d5befd0b75f738c6727c672d69fa5e54293b8c06
3men2kbson <lazarogs@gmail.com>
2016-11-16 23:19:58 -0400

    Agregados los modelos para Lectura Propia y Lectura Distribuidora

---
+++ ConsumoElectrico/Models/LecturaDistrib.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace ConsumoElectrico.Models
+{
+    public class LecturaDistrib
+    {
+        [DisplayName("Id")]
+        public int LDId { get; set;  }
+        [DisplayName("Fecha")]
+        public DateTime LDFecha { get; set; }
+        [DisplayName("Valor")]
+        public decimal LDLectura { get; set; }
+
+    }
+}
---
+++ ConsumoElectrico/Models/LecturaPropia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace ConsumoElectrico.Models
+{
+    public class LecturaPropia
+    {
+        [DisplayName("Id")]
+        public int LPId { get; set; }
+        [DisplayName("Fecha")]
+        public DateTime LPFecha { get; set; }
+        [DisplayName("Valor")]
+        public decimal LPLectura { get; set; }
+        [DisplayName("Subsidiado?")]
+        public Boolean LPSubsidio { get; set; }
+
+    }
+}
